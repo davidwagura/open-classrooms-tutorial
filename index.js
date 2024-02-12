5d6c8d237c10db1bfc3b15c41bf671aac54354bf
David wanjohi <davidwagura83@gmail.com>
2024-02-12 11:30:04 +0300

    Modifying the value of a variable

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-let firstName = 'David';
-let lastName = 'Wagura';
+let cakes = 10;
+let milk = 12;
+let sugar = 5;
 
+let totalPrice = cakes + milk;
+let balance = milk - cakes;
+let twoCakes = cakes * 2 ;
+
+//adding 1 
+cakes++;
+//subtracting 1
+cakes--;
+//division
+cakes / sugar
+
+let numberOfCats = 2;
+
+numberOfCats *= 6; // numberOfCats is now 2*6 = 12;
+
+numberOfCats /= 3; // numberOfCats is now 12/3 = 4;
+
+
+console.log('totalPrice');
