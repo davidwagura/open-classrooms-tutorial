ddac374bd4d50333772e95725e8037589d7b3ec1
David wanjohi <davidwagura83@gmail.com>
2024-02-14 19:32:47 +0300

    use of bollean

--- Javascript/index.js
+++ Javascript/index.js
@@ -12,3 +12,11 @@ console.log(typeof largeInteger);
 //floating-point arithmetic
 let integerCalculation = 1 + 2; // gives 3
 let wierdCalculation = 0.1 + 0.2; // 0.3 expected, actual answer 0.3000000
+
+// Booleans (true/false)
+
+let userSignedIn = true;
+let userSignedOut = false;
+
+console.log(typeof userSignedIn);
+console.log(typeof userSignedOut);
