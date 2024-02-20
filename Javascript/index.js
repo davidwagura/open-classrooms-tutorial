d454e94271929945c585d2139524fd6fd4be5eb4
David wanjohi <davidwagura83@gmail.com>
2024-02-20 06:16:36 +0300

    add object data

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,28 +1,7 @@
-//type number
-
-let positiveInteger = 23;
-let negatveInteger = -24;
-let largeDecimal = 42.2224;
-
-
-console.log(typeof positiveInteger);
-console.log(typeof negatveIntegerInteger);
-console.log(typeof largeInteger);
-
-//floating-point arithmetic
-let integerCalculation = 1 + 2; // gives 3
-let wierdCalculation = 0.1 + 0.2; // 0.3 expected, actual answer 0.3000000
-
-// Booleans (true/false)
-
-let userSignedIn = true;
-let userSignedOut = false;
-
-console.log(typeof userSignedIn);
-console.log(typeof userSignedOut);
-
-//string
-let firstName = ' John';
-let lastName = 'Maina';
-
-console.log(firstName + ' ' + lastName);
+//objects
+let myBook = {
+    title: 'The Story of Tau',
+    author: 'Will Alexander',
+    numberOfPages: 250,
+    isAvailable: true
+};
