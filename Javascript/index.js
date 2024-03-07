975d8cfddb563b59b2de110d373c7083eedbe08c
David wanjohi <davidwagura83@gmail.com>
2024-03-07 12:07:36 +0300

    javascript functions

--- Javascript/index.js
+++ Javascript/index.js
@@ -252,3 +252,16 @@ try {
 } catch {
     //react to error here
 }
+
+
+
+
+
+
+//FUNCTIONS
+const add = (firstNumber , secondNumber) => {
+    const result = firstNumber + secondNumber;
+    return result;
+}
+
+console.log(add(4,3));
