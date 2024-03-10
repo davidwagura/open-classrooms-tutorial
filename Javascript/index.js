a8b7d7427c7c839369f3c0435291cafaaed75df0
David wanjohi <davidwagura83@gmail.com>
2024-03-10 21:30:21 +0300

    static methods

--- Javascript/index.js
+++ Javascript/index.js
@@ -327,3 +327,23 @@ class BankAccount {
         }
     }
 }
+
+
+//static methods
+
+class BePolite {
+    static sayHello() {
+        console.log('Hello!');
+    }
+    static sayHello(name) {
+        console.log('Hello ' + name + '!');
+    }
+    static add(firstNumber, secondNumber) {
+        return firstNumber + secondNumber;
+    }
+}
+
+BePolite.sayHello(); //prints 'Hello!'
+BePolite.sayHello('Will'); //prints 'Hello Will!'
+
+const sum = BePolite.add(2, 3); //sum = 5
