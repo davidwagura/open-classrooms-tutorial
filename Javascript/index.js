6f5fe5b2162819b712839777bbbda7a10a606d5a
David wanjohi <davidwagura83@gmail.com>
2024-03-10 20:36:39 +0300

    add deposit and withdraw functions

--- Javascript/index.js
+++ Javascript/index.js
@@ -294,6 +294,36 @@ class BamkAccount {
     }
 }
 
-const newAccount = new BankAccount('Will Alexander', 500);
+// const newAccount = new BankAccount('Will Alexander', 500);
 
 newAccount.showBalance();
+
+
+///deposit(), withdraw()
+
+class BankAccount {
+    constructor(owner, balance) {
+        this.owner = owner;
+        this.balance = balance;
+    }
+    
+    showBalance() {
+        console.log('Balance: ' + this.balance + ' EUR');
+    }
+    
+    deposit(amount) {
+        console.log('Depositing ' + amount + ' EUR');
+        this.balance += amount;
+        this.showBalance();
+    }
+    
+    withdraw(amount) {
+        if (amount > this.balance) {
+            console.log('Withdrawal denied!');
+        } else {
+            console.log('Withdrawing ' + amount + ' EUR');
+            this.balance -= amount;
+            this.showBalance();
+        }
+    }
+}
