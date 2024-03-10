0e6ed0e30a02f8459bb8f68df7fb861db111b1b4
David wanjohi <davidwagura83@gmail.com>
2024-03-10 21:41:55 +0300

    write clean code

--- Javascript/index.js
+++ Javascript/index.js
@@ -347,3 +347,45 @@ BePolite.sayHello(); //prints 'Hello!'
 BePolite.sayHello('Will'); //prints 'Hello Will!'
 
 const sum = BePolite.add(2, 3); //sum = 5
+
+//writing clean functions
+
+if (firstUser.online) {
+    if (firstUser.accountType === 'normal') {
+      console.log('Hello ' + firstUser.name + '!');
+    } else {
+    console.log('Welcome back premium user ' + firstUser.name + '!');
+    }
+}
+
+if (secondUser.online) {
+    if (secondUser.accountType === 'normal') {
+      console.log('Hello ' + secondUser.name + '!');
+    } else {
+    console.log('Welcome back premium user ' + secondUser.name + '!');
+    }
+}
+
+if (thirdUser.online) {
+    if (thirdUser.accountType === 'normal') {
+      console.log('Hello ' + thirdUser.name + '!');
+    } else {
+    console.log('Welcome back premium user ' + thirdUser.name + '!');
+    }
+}
+
+
+///refactor code into functions
+const sendWelcomeMessageToUser = (user) => {
+    if (user.online) {
+        if (user.accountType === 'normal') {
+          console.log('Hello ' + user.name + '!');
+        } else {
+        console.log('Welcome back premium user ' + user.name + '!');
+        }
+    }
+}
+
+sendWelcomeMessageToUser(firstUser);
+sendWelcomeMessageToUser(secondUser);
+sendWelcomeMessageToUser(thirdUser);
